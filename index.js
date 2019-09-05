00ed4cfea354ad41e20f4afd892e888166687ed5
John Clarke <themagoo@hotmail.co.uk>
2019-09-05 22:50:32 +0100

    example code in index to get unique categories

--- index.js
+++ index.js
@@ -5,9 +5,21 @@ f = async (feed) => {
 
     let content = await parser.parseURL(feed)
 
+    let cats = []
+
     content.items.forEach(i => {
         console.log(`${i.title} : ${i.link}`)
+        if(i.categories && i.categories.length > 0)
+            i.categories.forEach(c => cats.push(c))
     });
+
+    let distinctCats = []
+    cats.forEach(c => {
+        if(distinctCats.find(f => f == c) == undefined)
+            distinctCats.push(c)
+    })
+
+    distinctCats.forEach(c => console.log(` [${c}] `))
 }
 
 f("https://medium.com/feed/@johnclarke_82232")
